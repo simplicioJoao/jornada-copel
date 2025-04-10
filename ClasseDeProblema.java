838ac0af4f177b265d77a19b68a29741969caecf
João Paulo Simplicio <135062516+simplicioJoao@users.noreply.github.com>
2025-04-10 20:18:40 -0300

    Adiciona estrutura da classe de problema

---
+++ ClasseDeProblema.java
@@ -0,0 +1,6 @@
+public class ClasseDeProblema {
+    private String cenario;
+    private String descricao;
+    public String tempoDeEspera; // formato "DD/MM/AAAA"
+    public int tempoMedioDeAtendimento;
+}
